9e5ab310cc61c8b3eeb3bbbab8d9bdbb1492259a
Ronny <ronaldsrundans@gmail.com>
2019-10-25 20:09:42 +0300

    Update Sum.php
    added null and reverse functions

--- Matrix/Sum.php
+++ Matrix/Sum.php
@@ -2,9 +2,8 @@
 <html>
 <body>
 
-<h2>Number Field</h2>
-<p>The <strong>input type="number"</strong> defines a numeric input field.</p>
-<p>You can use the min and max attributes to add numeric restrictions in the input field:</p>
+<h2>Matrix Calculator</h2>
+
 
 <form name="myForm">
   X:
@@ -12,34 +11,44 @@
   Y:
   <input type="number" name="inputB"id="B"><br>
 
- <input type="button" value="sum" onclick="myFunctionSum()">
-<input type="button" value="multiply" onclick="myFunctionMult()">
+ <input type="button" value="+" onclick="myFunctionSum()">
+<input type="button" value="*" onclick="myFunctionMult()">
 <input type="button" value="null" onclick="myFunctionNull()">
+<input type="button" value="<->" onclick="myFunctionRev()">
+
 
 </form>
 <p>Result :</p>
 <p id="result"></p>
 <script>
-  
 function myFunctionNull(){
 document.getElementById('A').value= 0;
 document.getElementById('B').value= 0;
+document.getElementById('result').innerHTML= 0;
+
 }
-  
+
 function myFunctionSum(){
 var x=Number(document.forms["myForm"]["inputA"].value);
 var y=Number(document.forms["myForm"]["inputB"].value);
+
 document.getElementById('result').innerHTML= (x+y);
 }
 
 function myFunctionMult(){
 var x=Number(document.forms["myForm"]["inputA"].value);
 var y=Number(document.forms["myForm"]["inputB"].value);
+
 document.getElementById('result').innerHTML= (x*y);
 }
 
+function myFunctionRev(){
+var tmp=Number(document.forms["myForm"]["inputA"].value);
+document.getElementById('A').value= Number(document.forms["myForm"]["inputB"].value);
+document.getElementById('B').value=Number(tmp); 
+
+  }
 </script>
 </body>
 </html
-
 
