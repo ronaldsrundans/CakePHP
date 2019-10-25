cf7361e54febac6cecba8bcff1e1481a88ad3690
Ronny <ronaldsrundans@gmail.com>
2019-10-25 12:13:19 +0300

    Create MatrixMatrix.php
    Does not calculate

---
+++ MatrixMatrix.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Matrix Calculator</title>
+</head>
+<body>
+
+<h1>Matrix calculator</h1>
+<p>Choose one operation:</p>
+<p>
+ <form>
+  <input type="radio" name="operation" value="+" checked> A+B=C<br>
+  <input type="radio" name="operation" value="-"> A-B=C<br>
+  <input type="radio" name="operation" value="*"> A*B=C
+</form>
+</p>
+<p>Input size of the matrix:</p><br>
+
+
+
+
+<p>
+ 
+<form>
+Matrix A:<br>
+<table>
+  <tr>
+    <td>  <input type="text" name="A00"></td>
+    <td>  <input type="text" name="A10"></td>
+    <td>  <input type="text" name="A20"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="A10"></td>
+    <td>  <input type="text" name="A11"></td>
+    <td>  <input type="text" name="A12"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="A20"></td>
+    <td>  <input type="text" name="A21"></td>
+    <td>  <input type="text" name="A22"></td>
+  </tr>
+</table>
+<br>
+
+</p>
+<br>
+<p> 
+
+Matrix B:<br>
+<table>
+  <tr>
+    <td>  <input type="text" name="B00"></td>
+    <td>  <input type="text" name="B10"></td>
+    <td>  <input type="text" name="B20"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="B10"></td>
+    <td>  <input type="text" name="B11"></td>
+    <td>  <input type="text" name="B12"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="B20"></td>
+    <td>  <input type="text" name="B21"></td>
+    <td>  <input type="text" name="B22"></td>
+  </tr>
+</table>
+<br>
+</form>
+</p>
+<br>
+<input type="submit" value="Calculate">
+
+<br>
+
+
+<form>
+Matrix C:<br>
+
+<table>
+  <tr>
+    <td>  <input type="text" name="C00"></td>
+    <td>  <input type="text" name="C10"></td>
+    <td>  <input type="text" name="C20"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="C10"></td>
+    <td>  <input type="text" name="C11"></td>
+    <td>  <input type="text" name="C12"></td>
+  </tr>
+  <tr>
+    <td>  <input type="text" name="C20"></td>
+    <td>  <input type="text" name="C21"></td>
+    <td>  <input type="text" name="C22"></td>
+  </tr>
+</table>
+<br>
+</form>
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html> 
+
