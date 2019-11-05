00a9f2d421264501574298e6126e4fea4419731f
Ronny <ronaldsrundans@gmail.com>
2019-11-05 20:13:15 +0200

    Using only Javascript. "Test" button sums A and B
    Using only Javascript. "Test" button sums A and B

--- Matrix/Sum.php
+++ Matrix/Sum.php
@@ -1,54 +1,91 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h2>Matrix Calculator</h2>
-
-
-<form name="myForm">
-  X:
-  <input type="number" name="inputA" id="A"><br>
-  Y:
-  <input type="number" name="inputB"id="B"><br>
-
- <input type="button" value="+" onclick="myFunctionSum()">
-<input type="button" value="*" onclick="myFunctionMult()">
-<input type="button" value="null" onclick="myFunctionNull()">
-<input type="button" value="<->" onclick="myFunctionRev()">
-
-
-</form>
-<p>Result :</p>
-<p id="result"></p>
 <script>
-function myFunctionNull(){
-document.getElementById('A').value= 0;
-document.getElementById('B').value= 0;
-document.getElementById('result').innerHTML= 0;
+function createTableA()
+{
+    var num_rows = document.getElementById('rowsA').value;
+    var num_cols = document.getElementById('colsA').value;
+    var theader = '<table border="1">';
+    var tbody = '';
+    for( var i=0; i<num_rows;i++)
+    {
+        tbody += '<tr>';
+        for( var j=0; j<num_cols;j++)
+        {
+            tbody += '<td>';
+            tbody += '<input type="number" id = A' + i + j+' >';
+            tbody += '</td>'
+        }
+        tbody += '</tr>';
+    }
+    var tfooter = '</table>';
+    document.getElementById('wrapperA').innerHTML = theader + tbody + tfooter;
+}
+function createTableB()
+{
+    var num_rows = document.getElementById('rowsB').value;
+    var num_cols = document.getElementById('colsB').value;
+    var theader = '<table border="1">\n';
+    var tbody = '';
+
+    for( var i=0; i<num_rows;i++)
+    {
+        tbody += '<tr>';
+        for( var j=0; j<num_cols;j++)
+        {
+            tbody += '<td>';
+            tbody += '<input type="number" id = B' + i + j+' >';
+            tbody += '</td>'
+
+        }
+        tbody += '</tr>\n';
+    }
+    var tfooter = '</table>';
+    document.getElementById('wrapperB').innerHTML = theader + tbody + tfooter;
+}
+function myMatrix(){
+    var num_rows = document.getElementById('rowsA').value;
+    var num_cols = document.getElementById('colsB').value;
+    var theader = '<table border="1">';
+    var tbody = '';
+
+
+for( var i=0; i<num_rows;i++)
+    {
+        tbody += '<tr>';
+        for( var j=0; j<num_cols;j++)
+        {
+            var a = Number(document.getElementById('A'+i+j).value);
+            var b = Number(document.getElementById('B'+i+j).value);
+            tbody += '<td>';
+            tbody += (a+b);
+            tbody += '</td>'
+
+        }
+        tbody += '</tr>\n';
+    }
+    var tfooter = '</table>';
+    document.getElementById('result').innerHTML = theader + tbody + tfooter;
 
 }
-
-function myFunctionSum(){
-var x=Number(document.forms["myForm"]["inputA"].value);
-var y=Number(document.forms["myForm"]["inputB"].value);
-
-document.getElementById('result').innerHTML= (x+y);
-}
-
-function myFunctionMult(){
-var x=Number(document.forms["myForm"]["inputA"].value);
-var y=Number(document.forms["myForm"]["inputB"].value);
-
-document.getElementById('result').innerHTML= (x*y);
-}
-
-function myFunctionRev(){
-var tmp=Number(document.forms["myForm"]["inputA"].value);
-document.getElementById('A').value= Number(document.forms["myForm"]["inputB"].value);
-document.getElementById('B').value=Number(tmp); 
-
-  }
 </script>
-</body>
-</html
+</head>
 
+<body>
+<form name="tablegenA">
+<label>Rows: <input type="text" name="rowsA" id="rowsA"></label>
+<label>Cols: <input type="text" name="colsA" id="colsA"></label><br>
+<input name="generate" type="button" value="Create Table A!" onclick='createTableA();'>
+</form>
+
+<div id="wrapperA"></div>
+<form name="tablegenB">
+<label>Rows: <input type="text" name="rowsB" id="rowsB"></label>
+<label>Cols: <input type="text" name="colsB" id="colsB"></label><br>
+<input name="generate" type="button" value="Create Table B!" onclick='createTableB();'>
+</form>
+
+<div id="wrapperB"></div>
+<button onclick="myMatrix()">Test</button>
+
+<div id="result"></div>
+</body>
+</html>
