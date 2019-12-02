99614b78c7e8da388c2afc4163ebce7dca7f0844
Ronny <ronaldsrundans@gmail.com>
2019-12-02 15:01:47 +0200

    Update inos.php

--- homeDB/inos.php
+++ homeDB/inos.php
@@ -10,8 +10,8 @@
 
 <?php
 // define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
+$nameErr = $manErr = "";
+$name = $man= = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
