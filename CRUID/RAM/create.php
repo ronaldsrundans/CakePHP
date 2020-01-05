6b845b9825a75e2d7687318cf97a50e84e7d67ec
Ronny <ronaldsrundans@gmail.com>
2020-01-05 10:14:46 +0200

    Update create.php

--- CRUID/RAM/create.php
+++ CRUID/RAM/create.php
@@ -1,5 +1,4 @@
 <?php
-
 // Include config file
 require_once "config.php";
  
@@ -8,10 +7,8 @@ $man =$name = $freq = $mem = $type = "";
 $man_err =$name_err = $freq_err = $mem_err = $type_err = "";
  
 // Processing form data when form is submitted
-if(isset($_POST["id"]) && !empty($_POST["id"])){
-    // Get hidden input value
- /*   $id = $_POST["id"];
-	$input_man = trim($_POST["man"]);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 	$input_man = trim($_POST["man"]);
 	$input_name = trim($_POST["name"]);
 	$input_mem = trim($_POST["mem"]);
     $input_freq = trim($_POST["freq"]);
@@ -22,8 +19,8 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
 	$mem = $input_mem;
 	$freq = $input_freq;
 	$type = $input_type;
-    /*
     // Validate name
+	/*
     $input_name = trim($_POST["name"]);
     if(empty($input_name)){
         $name_err = "Please enter a name.";
@@ -32,16 +29,18 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     } else{
         $name = $input_name;
     }
-    
-    // Validate address address
+    */
+    // Validate address
+	/*
     $input_address = trim($_POST["address"]);
     if(empty($input_address)){
         $address_err = "Please enter an address.";     
     } else{
         $address = $input_address;
     }
-    
+    */
     // Validate salary
+	/*
     $input_salary = trim($_POST["salary"]);
     if(empty($input_salary)){
         $salary_err = "Please enter the salary amount.";     
@@ -50,27 +49,26 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     } else{
         $salary = $input_salary;
     }
-    
+    */
     // Check input errors before inserting in database
-    if(empty($name_err) && empty($man_err) && empty($freq_err)&& empty($mem_err)&& empty($type_err)){
-        // Prepare an update statement
-        $sql = "UPDATE RAM SET man=?, name=?, mem=?, freq=?, type=? WHERE id=?";
+    if(empty($name_err) && empty($man_err) && empty($mem_err)&& empty($freq_err)&& empty($type_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO RAM (ramman, ramname, ramfreq, rammem, ramtype) VALUES (?, ?, ?, ?, ?)";
          
-        if($stmt = mysqli_prepare($link, $sql)
+        if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sssssi",$param_man, $param_name, $param_mem, $param_freq, $param_type, $param_id);
+            mysqli_stmt_bind_param($stmt, "sssss", $param_man, $param_name, $param_freq, $param_mem, $param_type);
             
             // Set parameters
             $param_man = $man;
-            $param_name = $name;            
+            $param_name = $name;
+            $param_freq = $freq;
 			$param_mem = $mem;
-			$param_freq = $freq;
             $param_type = $type;
-            $param_id = $id;
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
-                // Records updated successfully. Redirect to landing page
+                // Records created successfully. Redirect to landing page
                 header("location: index.php");
                 exit();
             } else{
@@ -83,67 +81,15 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     }
     
     // Close connection
-    mysqli_close($link);*/
-} 
-//else{
-    // Check existence of id parameter before processing further
-    if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-        // Get URL parameter
-        $id =  trim($_GET["id"]);
-        
-        // Prepare a select statement
-        $sql = "SELECT * FROM RAM WHERE id = ?";
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "i", $param_id);
-            
-            // Set parameters
-            $param_id = $id;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                $result = mysqli_stmt_get_result($stmt);
-    
-                if(mysqli_num_rows($result) == 1){
-                   // Fetch result row as an associative array. Since the result set contains only one row, we don't need to use while loop 
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                    
-                    // Retrieve individual field value
-                    $man = $row["man"];
-					$name = $row["name"];
-					$mem = $row["mem"];
-					$freq = $row["freq"];
-					$type = $row["type"];
-                  
-                } else{
-                    // URL doesn't contain valid id. Redirect to error page
-                    header("location: error.php");
-                    exit();
-                }
-                
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-        }
-        
-        // Close statement
-        mysqli_stmt_close($stmt);
-        
-        // Close connection
-        mysqli_close($link);
-    }  else{
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-    }
-//}
+    mysqli_close($link);
+}
 ?>
  
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Update Record</title>
+    <title>Create Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -158,10 +104,10 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h2>Update Record</h2>
+                        <h2>Create Record</h2>
                     </div>
-                    <p>Please edit the input values and submit to update the record.</p>
-                    <form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+                    <p>Please fill this form and submit to add RAM record to the database.</p>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group <?php echo (!empty($man_err)) ? 'has-error' : ''; ?>">
                             <label>Manufacturer</label>
                             <input type="text" name="man" class="form-control" value="<?php echo $man; ?>">
@@ -187,7 +133,6 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
                             <input type="text" name="type" class="form-control" value="<?php echo $type; ?>">
                             <span class="help-block"><?php echo $type_err;?></span>
                         </div>
-                        <input type="hidden" name="id" value="<?php echo $id; ?>"/>
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
                     </form>
