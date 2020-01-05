95097f28cffa432480526e59f877a58132093858
Ronny <ronaldsrundans@gmail.com>
2020-01-05 10:15:56 +0200

    Update delete.php

--- CRUID/RAM/delete.php
+++ CRUID/RAM/delete.php
@@ -5,7 +5,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
     require_once "config.php";
     
     // Prepare a delete statement
-    $sql = "DELETE FROM RAM WHERE id = ?";
+    $sql = "DELETE FROM RAM WHERE ramid = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
