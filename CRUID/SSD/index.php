5d62ffaec04ab50b654eef8250c2136dddd6ad3d
Ronny <ronaldsrundans@gmail.com>
2020-01-10 08:46:55 +0200

    Update index.php

--- CRUID/SSD/index.php
+++ CRUID/SSD/index.php
@@ -31,7 +31,7 @@
                 <div class="col-md-12">
                     <div class="page-header clearfix">
                         <h2 class="pull-left">SSD Details</h2>
-                        <a href="create.php" class="btn btn-success pull-right">Add New CPU</a>
+                        <a href="create.php" class="btn btn-success pull-right">Add New SSD</a>
                     </div>
                     <?php
                     // Include config file
