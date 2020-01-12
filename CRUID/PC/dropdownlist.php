b09fcf485cb53e838f0626c5b97a0ed9c26d9f37
Ronny <ronaldsrundans@gmail.com>
2020-01-12 15:33:18 +0200

    Update dropdownlist.php

--- CRUID/PC/dropdownlist.php
+++ CRUID/PC/dropdownlist.php
@@ -2,36 +2,31 @@
 <!-- Put you mysqli connection here ................ -->
 <title>PHP SAMPLE SYSTEM</title>
 <header>
-<h1 align="center">Populate Drop Down List From Database Using PHP and MySQLi</h1>
+<h1>Select CPU</h1>
 </header>
 <!-- Put your style layout here...... -->
 <body>
-<div id="box">
-<div class="column"></div>
-<div class="column">
-<div class="clear"></div>
-<label class="first-column ">Select a Book:</label><div class="clear"></div>
-<div class="clear"></div>
-ate the dropdown list codePHP
+<form action="/action_page.php" id="cpuform">
+ 
+  <input type="submit">
+</form>
 <?php
     // Include config file
     require_once "config.php";
 
-echo '<select>
-<option>Select</option>';
+echo '<select name="cpulist" form="cpuform">';
+
 
 
 $sqli = "SELECT * FROM CPU";
 $result = mysqli_query($con, $sqli);
 while ($row = mysqli_fetch_array($result)) {
-echo '<option>'.$row['cpuname'].'</option>';
+echo '<option value="'.$row['cpuid'].'">'.$row['cpuname'].'</option>';
 }
  
 echo '</select>';
  
 ?>
-</div>
-<div class="column"></div>
-</div>
+
 </body>
 </html>
