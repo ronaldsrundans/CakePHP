efb56273394f75c286bdf3d65d4a773008731d5e
Ronny <ronaldsrundans@gmail.com>
2020-01-15 09:15:00 +0200

    Update create.php

--- CRUID/ANIME/create.php
+++ CRUID/ANIME/create.php
@@ -3,25 +3,25 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$title =$type = "";
-$title_err =$type_err = "";
+$type =$title = "";
+$type_err =$title_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
  	$input_title = trim($_POST["title"]);
 	$input_type = trim($_POST["type"]);
 	
-	$title = $input_title;
-	$type = $input_type;
+	$type = $input_title;
+	$title = $input_type;
 	// Validate name
 	/*
     $input_type = trim($_POST["name"]);
     if(empty($input_type)){
-        $type_err = "Please enter a name.";
+        $title_err = "Please enter a name.";
     } elseif(!filter_var($input_type, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $type_err = "Please enter a valid name.";
+        $title_err = "Please enter a valid name.";
     } else{
-        $type = $input_type;
+        $title = $input_type;
     }
     */
     // Validate address
@@ -45,7 +45,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     */
     // Check input errors before inserting in database
-    if(empty($type_err) && empty($title_err)){
+    if(empty($title_err) && empty($type_err)){
         // Prepare an insert statement
         $sql = "INSERT INTO MYANIMELIST (title, type) VALUES (?, ?)";
          
@@ -54,8 +54,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_bind_param($stmt, "ss", $param_title, $param_type);
             
             // Set parameters
-            $param_title = $title;
-            $param_type = $type;
+            $param_title = $type;
+            $param_type = $title;
                         
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -80,7 +80,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create OS Record</title>
+    <title>Create ANIME Record</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -99,16 +99,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
                     <p>Please fill this form and submit to add OS record to the database.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="form-group <?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
+                           <div class="form-group <?php echo (!empty($title_err)) ? 'has-error' : ''; ?>">
                             <label>Title</label>
                             <input type="text" name="title" class="form-control" value="<?php echo $title; ?>">
                             <span class="help-block"><?php echo $title_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($type_err)) ? 'has-error' : ''; ?>">
-                            <label>Typeos</label>
+                        </div> 
+						<div class="form-group <?php echo (!empty($type_err)) ? 'has-error' : ''; ?>">
+                            <label>Type</label>
                             <input type="text" name="type" class="form-control" value="<?php echo $type; ?>">
                             <span class="help-block"><?php echo $type_err;?></span>
                         </div>
+                    
                         <input type="submit" class="btn btn-primary" value="Submit">
                         <a href="index.php" class="btn btn-default">Cancel</a>
                     </form>
