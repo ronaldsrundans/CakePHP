9924515c3e57c2047346d996c4c05de3c847c468
Ronny <ronaldsrundans@gmail.com>
2020-04-29 21:35:32 +0300

    Create get_table.php

---
+++ ToDoList/get_table.php
@@ -0,0 +1,21 @@
+<?php
+$contents = '<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>First Name</th>
+<th>Last Name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>John</td>
+<td>Deo</td>
+        </tr>
+<tr>
+            <td>Tum</td>
+<td>Deo</td>
+        </tr>
+    </tbody>
+</table>';
+echo json_encode($contents);
+?>
