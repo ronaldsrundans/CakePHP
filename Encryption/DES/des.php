856a81683c7d63ef544539bb0116f19511d5aef1
Ronny <ronaldsrundans@gmail.com>
2020-05-03 22:58:46 +0300

    Update des.php

--- Encryption/DES/des.php
+++ Encryption/DES/des.php
@@ -25,20 +25,29 @@
 
 <script>
 var x = "Your lips are smoother than vaseline."
-var y = "8787878787878787";
-var key ="0E329232EA6D0D73";
+var y = "0123456789ABCDEF";
+var key ="133457799BBCDFF1";
 //document.getElementById("plaintext").innerHTML = x;
 document.getElementById("text2hex").innerHTML = y;
 document.getElementById("key2hex").innerHTML = key;
 document.getElementById("text2bin").innerHTML = hex2bin(y);
 document.getElementById("key2bin").innerHTML = hex2bin(key);
 document.getElementById("printArr").innerHTML = des(hex2bin(y),hex2bin(key));
+//document.getElementById("printArr").innerHTML = myTest();
 //des(hex2bin(y),hex2bin(key));
 document.getElementById("bin2hex").innerHTML = bin2hex(hex2bin(y));
 document.getElementById("binkey2hex").innerHTML = bin2hex(hex2bin(key));
 
 document.getElementById("bin2text").innerHTML = hex2text(bin2hex(hex2bin(y)));
+function myTest(){
+const items = [1, 29, 47];
+const copy = [];
 
+items.forEach(function(item){
+  copy.push(item*item);
+});
+return copy;
+}
 function text2hex(str) {
     var hex = '';
     var hexPadding, hexLen, i;
@@ -126,23 +135,7 @@ function bin2dec(binarr){
 }
 
 /*
-function shift(arr, n){
- 	tmp1,tmp2,tmp3;
-	tmp1=arr[0];
-	tmp2=arr[1];
-	tmp3=arr[27];
-	for( k=0;k<26;k++){
-		arr[k]=arr[k+n];
-	}
-	if(n==2){
-		arr[26]=tmp1;
-		arr[27]=tmp2;
-	}
-	else if(n==1){
-		arr[27]=tmp1;
-		arr[26]=tmp3;
-	}
-}
+
 function deshift(arr, n){
 	tmp1,tmp2,tmp3;
 	tmp1=arr[0];
@@ -160,9 +153,12 @@ function deshift(arr, n){
 		arr[0]=tmp3;
 	}
 }
-function xorfunc( arr1,  arr2, arr3,  n){
+
+
+*/
+function xorFunction( arr1,  arr2, arr3,  n){
 	for( i=0;i<n;i++){
-		if(((arr1[i]==1) or (arr2[i]==1))&& (arr1[i]!= arr2[i])){
+		if(((arr1[i]==1) | (arr2[i]==1))& (arr1[i]!= arr2[i])){
 			arr3[i]=1;
 		}
 		else{
@@ -171,24 +167,25 @@ function xorfunc( arr1,  arr2, arr3,  n){
 	}
 }
 
+function leftShift(arr, n){
+	var tmp;
+	for(var i=0; i<n; i++){
+		tmp=arr[0];//save first element
+		arr.shift();//removes first element
+		arr.push(tmp);//pushes first element at the end
+	}
+}
+function splitArr(arr, arr1, arr2,  n){
+	for( i=0;i<n;i++){
+		arr1[i]=arr[i];
+		arr2[i]=arr[i+n];
+	}
+}
 function permutation( n, arr1,  arr2,  arrp){
  	for(var i=0;i<n;i++){
 		arr2[i] = arr1[arrp[i]-1];
 	}
 }
-function dectobin( dec, bin){
- 	tmp=dec;
-	for( i=3;i>=0;i--){
-		bin[i]=tmp%2;
-		tmp=tmp/2;
-	}
-}
-function split(arr, arr1, arr2,  n){
-	for( i=0;i<n;i++){
-		arr1[i]=arr[i];
-		arr2[i]=arr[i+n];
-	}
-}*/
 function des(binTxt, binKey){
 	var ip = [58,50,42,34,26,18,10,2,60,52,44,36,28,20,12,4,
               62,54,46,38,30,22,14,6,64,56,48,40,32,24,16,8,
@@ -204,6 +201,7 @@ function des(binTxt, binKey){
               23,19,12, 4,26, 8,16, 7,27,20,13, 2,
               41,52,31,37,47,55,30,40,51,45,33,48,
               44,49,39,56,34,53,46,42,50,36,29,32];
+              
     var ep = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9,
     		   8, 9,10,11,12,13,12,13,14,15,16,17,
     		  16,17,18,19,20,21,20,21,22,23,24,25,
@@ -224,22 +222,22 @@ function des(binTxt, binKey){
                  [ 0,14, 7,11,10, 4,13, 1, 5,8,12, 6, 9,3, 2,15],
                  [13, 8,10, 1, 3,15, 4, 2,11,6, 7,12, 0,5,14, 9]];
 
-    var sbox3 = [[10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
-                 [13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
-                 [13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
-                 [1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]];
+    var sbox3 = [[10, 0, 9,14,6, 3,15,5,1,13,12,7,11,4,2,8],
+                 [13, 7, 0, 9,3, 4, 6,10,2,8,5,14,12,11,15,1],
+                 [13, 6, 4, 9,8,15, 3,0,11,1,2,12,5,10,14,7],
+                 [ 1,10,13, 0,6, 9, 8,7,4,15,14,3,11,5,2,12]];
 
-    var sbox4 = [[ 7,13,14,3, 0,6,9,10,1,2,8,5,11,12,4,15],
-                 [13, 8,11,5, 6,15,0,3,4,7,2,12,1,10,14,9],
-                 [10, 6, 9,0,12,11,7,13,15,1,3,14,5,2,8,4],
-                 [ 3,15, 0,6,10,1,13,8,9,4,5,11,12,7,2,14]];
+    var sbox4 = [[ 7,13,14,3, 0, 6, 9,10, 1,2,8, 5,11,12, 4,15],
+                 [13, 8,11,5, 6,15, 0, 3, 4,7,2,12, 1,10,14, 9],
+                 [10, 6, 9,0,12,11, 7,13,15,1,3,14, 5, 2, 8, 4],
+                 [ 3,15, 0,6,10, 1,13, 8, 9,4,5,11,12, 7, 2,14]];
 
     var sbox5 = [[ 2,12, 4, 1, 7,10,11, 6, 8, 5, 3,15,13,0,14, 9],
                  [14,11, 2,12, 4, 7,13, 1, 5, 0,15,10, 3,9, 8, 6],
                  [ 4, 2, 1,11,10,13, 7, 8,15, 9,12, 5, 6,3, 0,14],
                  [11, 8,12, 7, 1,14, 2,13, 6,15, 0, 9,10,4, 5, 3]];
 
-    var sbox6=[[12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
+    var sbox6 = [[12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
                [10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
                [9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
                [4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]];
@@ -255,17 +253,45 @@ function des(binTxt, binKey){
                [2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]];
 
     var pbox=[16,7,20,21,29,12,28,17,1,15,23,26,5,18,31,10,2,8,24,14,32,27,3,9,19,13,30,6,22,11,4,25];              
-    var r = [];
-	var l = [];
+    var rn = [];
+	var ln = [];
 	var i;
 	var c = [];
 	var d = [];
-
-	//binTxt.forEach(
-	//var txtLen=(binTxt.lenght)/2;
-
-	//document.getElementById("demo").innerHTML = binm.toString();
-	return binKey;
+	var ipTxt=[];
+	var kpKey=[];
+	var kpKeyL=[];
+	var kpKeyR=[];
+	var fKey=[];
+	var cpKey=[];
+	var xorExp=[];
+	var rnExp=[];
+	var mLen=binTxt.length/2;
+    permutation(64, binTxt, ipTxt, ip);
+    ///key permutationS
+    permutation(56, binKey, kpKey, kp);
+    splitArr(kpKey,kpKeyL,kpKeyR,28);
+	///F function
+	for(i=0; i<1; i++){//change to 16
+		splitArr(ipTxt,ln,rn,32);
+		leftShift(kpKeyL,bsh[i]);
+        leftShift(kpKeyR,bsh[i]);
+        for(j=0;j<28;j++){
+            fKey[j]=kpKeyL[j];
+            fKey[j+28]=kpKeyR[j];
+        }
+        ///fkey compression permutation
+		permutation(48, fKey, cpKey, cp);
+		///Expantion Permutation
+ 		permutation(48, rn, rnExp, ep);
+ 		/// XOR (RNEXP,CPKEY)
+ 		xorFunction(rnExp,cpKey,xorExp,48);
+ 		///Sbox substitution
+ 		
+		
+	}
+	//document.getElementById("demo").innerHTML = rnExp;
+	return xorExp;
 }
 /*	
 	///cpp code
