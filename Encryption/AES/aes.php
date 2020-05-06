e265463d39b9589f757b67db3b41b434084915fc
Ronny <ronaldsrundans@gmail.com>
2020-05-06 20:32:31 +0300

    Update aes.php

--- Encryption/AES/aes.php
+++ Encryption/AES/aes.php
@@ -31,6 +31,22 @@ var key ="Thats my Kung Fu";
 var aeskey128="2b7e151628aed2a6abf7158809cf4f3c";
 var aeskey192="8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7b";
 var aeskey256="603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4";
+var sboxfix=[['63','7c','77','7b','f2','6b','6f','c5','30','01','67','2b','fe','d7','ab','76'],
+             ['ca','82','c9','7d','fa','59','47','f0','ad','d4','a2','af','9c','a4','72','c0'],
+             ['b7','fd','93','26','36','3f','f7','cc','34','a5','e5','f1','71','d8','31','15'],
+             ['04','c7','23','c3','18','96','05','9a','07','12','80','e2','eb','27','b2','75'],
+             ['09','83','2c','1a','1b','6e','5a','a0','52','3b','d6','b3','29','e3','2f','84'],
+             ['53','d1','00','ed','20','fc','b1','5b','6a','cb','be','39','4a','4c','58','cf'],
+             ['d0','ef','aa','fb','43','4d','33','85','45','f9','02','7f','50','3c','9f','a8'],
+             ['51','a3','40','8f','92','9d','38','f5','bc','b6','da','21','10','ff','f3','d2'],
+             ['cd','0c','13','ec','5f','97','44','17','c4','a7','7e','3d','64','5d','19','73'],
+             ['60','81','4f','dc','22','2a','90','88','46','ee','b8','14','de','5e','0b','db'],
+             ['e0','32','3a','0a','49','06','24','5c','c2','d3','ac','62','91','95','e4','79'],
+             ['e7','c8','37','6d','8d','d5','4e','a9','6c','56','f4','ea','65','7a','ae','08'],
+             ['ba','78','25','2e','1c','a6','b4','c6','e8','dd','74','1f','4b','bd','8b','8a'],
+             ['70','3e','b5','66','48','03','f6','0e','61','35','57','b9','86','c1','1d','9e'],
+             ['e1','f8','98','11','69','d9','8e','94','9b','1e','87','e9','ce','55','28','df'],
+             ['8c','a1','89','0d','bf','e6','42','68','41','99','2d','0f','b0','54','bb','16']];
 var testarr=[[1,2,3,4],
 [2,3,4,0],
 [3,9,1,2],
@@ -42,7 +58,8 @@ document.getElementById("text2bin").innerHTML = hex2bin(text2hex(plain));
 document.getElementById("key2bin").innerHTML = hex2bin(text2hex(key));
 //document.getElementById("printArr").innerHTML = bin2hex(des(hex2bin(y),hex2bin(key)));
 //document.getElementById("printArr").innerHTML = testarr;
-document.getElementById("printArr").innerHTML = keyExpansion(aeskey256);
+//document.getElementById("printArr").innerHTML = keyExpansion(aeskey256);
+document.getElementById("printArr").innerHTML =subWord("cf4f3c09");
 
 //document.getElementById("printArr").innerHTML = myTest();
 //des(hex2bin(y),hex2bin(key));
@@ -393,16 +410,55 @@ function keyExpansion(hexKey){
   var i=0;///dec
   var nk=hexKey.length/8;
   var rCon=i/nk;
-  w=[];
-  testarr=[1,2,3,4];
-  testarr2=[5,6,7,8]
+  var w=[];
   for(i=0;i<nk;i++){
     w.push(hexKey.slice(i*8,(i+1)*8));
   }
+  var afterRotWord;
+  var afterSubWord;
+  rotWord(w[0],afterRotWord);
+  //w[0]
 
   //w.push(hexKey.slice(0, 7));
 
   return i;
+}
+function rotWord(wordStr,rotStr){
+  var tmp1=wordStr.slice(0,2);
+  var tmp2=wordStr.slice(2,8);
+  rotStr=tmp2.concat(tmp1);
+  return rotStr;
+}
+function subWord(wordStr, afterStr){
+  var sbox  = [['63','7c','77','7b','f2','6b','6f','c5','30','01','67','2b','fe','d7','ab','76'],
+               ['ca','82','c9','7d','fa','59','47','f0','ad','d4','a2','af','9c','a4','72','c0'],
+               ['b7','fd','93','26','36','3f','f7','cc','34','a5','e5','f1','71','d8','31','15'],
+               ['04','c7','23','c3','18','96','05','9a','07','12','80','e2','eb','27','b2','75'],
+               ['09','83','2c','1a','1b','6e','5a','a0','52','3b','d6','b3','29','e3','2f','84'],
+               ['53','d1','00','ed','20','fc','b1','5b','6a','cb','be','39','4a','4c','58','cf'],
+               ['d0','ef','aa','fb','43','4d','33','85','45','f9','02','7f','50','3c','9f','a8'],
+               ['51','a3','40','8f','92','9d','38','f5','bc','b6','da','21','10','ff','f3','d2'],
+               ['cd','0c','13','ec','5f','97','44','17','c4','a7','7e','3d','64','5d','19','73'],
+               ['60','81','4f','dc','22','2a','90','88','46','ee','b8','14','de','5e','0b','db'],
+               ['e0','32','3a','0a','49','06','24','5c','c2','d3','ac','62','91','95','e4','79'],
+               ['e7','c8','37','6d','8d','d5','4e','a9','6c','56','f4','ea','65','7a','ae','08'],
+               ['ba','78','25','2e','1c','a6','b4','c6','e8','dd','74','1f','4b','bd','8b','8a'],
+               ['70','3e','b5','66','48','03','f6','0e','61','35','57','b9','86','c1','1d','9e'],
+               ['e1','f8','98','11','69','d9','8e','94','9b','1e','87','e9','ce','55','28','df'],
+               ['8c','a1','89','0d','bf','e6','42','68','41','99','2d','0f','b0','54','bb','16']];
+  //var l=wordStr.length;
+  var i;
+  var x=[];
+  var y=[];
+  for(var i=0;i<8;i+=2){
+    x.push(hex2dec(wordStr.slice(i,i+1)));
+    y.push(hex2dec(wordStr.slice(i+1,i+2)));
+  }
+  afterStr='';
+  for(i=0;i<4;i++){
+    afterStr+=sbox[x[i]][y[i]];
+  }
+  return afterStr;
 }
 </script>
 
